12636849f3cba6ee8c70812eeb8c063328bd4e4f
Tiago Freitas <70216659+FreitsTiago@users.noreply.github.com>
2020-10-28 19:38:39 -0300

    Animei as barras de progresso

--- public/src/scripts/scroll-anim.js
+++ public/src/scripts/scroll-anim.js
@@ -1,24 +1,31 @@
 // Debounce do Lodash
-debounce = function(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
+debounce = function (func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
 };
 
 // Função da animação
 (function () {
     var $target = $('.animation'),
+        progress = $('.rUln88GN'),
         animationClass = 'anime',
         offset = $(window).height() * 3 / 4;
+
+    var bars = [{ name: 'bar-1', value: '35' },
+    { name: 'bar-2', value: '52' },
+    { name: 'bar-3', value: '36' },
+    { name: 'bar-4', value: '57' },
+    { name: 'bar-5', value: '68' }];
 
     function animeScroll() {
         var documentTop = $(document).scrollTop();
@@ -30,6 +37,20 @@ debounce = function(func, wait, immediate) {
                 $(this).removeClass(animationClass);
             }
         })
+        if (progress.hasClass('anime')) {
+            setTimeout(function () {
+                for (bar of bars) {
+                    $('.' + bar.name).css('width', bar.value + '%');
+                    $('.' + bar.name).text(bar.value + '%');
+                }
+            }, 300)
+
+        } else {
+            for (bar of bars) {
+                $('.' + bar.name).css('width', 0 + '%');
+                $('.' + bar.name).text('');
+            }
+        }
     }
 
     animeScroll();
