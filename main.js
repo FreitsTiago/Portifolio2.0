fa542617f436b4cd32b6391f32acec94a9c3e2f7
Tiago Freitas <70216659+FreitsTiago@users.noreply.github.com>
2021-01-09 11:35:09 -0300

    New Server
    Servidor refeito agora usando express.

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-const http = require('http').createServer(servidor);
+const app = require('express')
+const http = require('http').Server(app);
 const fs = require('fs');
 
 const dir = __dirname + '/public'
 
-function servidor(req, res) {
-    console.log('Novo request: ', req.url)
-    if (req.url == '/') {
-        var resp = fs.readFileSync(dir + '/index.html')
-        res.end(resp);
-    } else {
-        try{
-            try{
-                var resp = fs.readFileSync(dir + req.url + '.html')
-            }catch(err){
-                var resp = fs.readFileSync(dir + req.url)
-            }
-        }catch(err){
-            var resp = fs.readFileSync(dir + '/404.html')
+app.get('/', function (req, res) {
+    var content = fs.readFileSync(__dirname + "/index.html");
+    res.end(content);
+});
+
+app.use('/files', function (req, res) {
+    try {
+        try {
+            var content = fs.readFileSync(__dirname + req.url + '.html');
+            res.end(content);
+        } catch {
+            var content = fs.readFileSync(__dirname + req.url);
+            res.end(content);
         }
-        res.end(resp);
+    } catch {
+        var content = fs.readFileSync(__dirname + '/404.html');
+        res.status(404);
+        res.end(content);
     }
-};
+});
 
 http.listen(process.env.PORT || 19132, function () {
     console.log("Server is online");
-});
+  });
